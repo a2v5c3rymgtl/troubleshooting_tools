e9e3decfb35503c08812ec69080f3eb1beab91df
Nickolay <00x00h@gmail.com>
2019-10-26 18:19:58 +0300

    moved all regexps to config

--- logs_visualizer.py
+++ logs_visualizer.py
@@ -5,14 +5,17 @@ from anytree.exporter import DotExporter
 from uuid import uuid4
 import re
 import sys
+from config import GENERAL_MESSAGE_PATTERN, COMPLETE_MESSAGE_PATTERN, COMMENCE_MESSAGE_PATTERN
+from config import SCOPE_NAME_GROUP, MESSAGE_LEVEL_GROUP
 
 
 class CallGraph:
     """
-    TODO think up about execution order and represent it something usefully and clearly
+    TODO think up about the execution order and represent it something usefully and clearly
     TODO implement text render with collected_info data
     TODO implement image render with collected_info data
     TODO add colors in text render
+    TODO add an animation creation functionality
     """
     current_node: Node
     previous_scope: Node
@@ -58,24 +61,30 @@ class CallGraph:
         DotExporter(self.main_scope).to_picture(picture_name)
 
 
+def is_valid_message(line: str) -> bool:
+    return bool(re.match(GENERAL_MESSAGE_PATTERN, line))
+
+
+def is_commence_message(line) -> bool:
+    return bool(re.match(COMMENCE_MESSAGE_PATTERN, line))
+
+
+def is_complete_message(line) -> bool:
+    return bool(re.match(COMPLETE_MESSAGE_PATTERN, line))
+
+
+def get_scope_name(line) -> str:
+    return re.match(GENERAL_MESSAGE_PATTERN, line).group(SCOPE_NAME_GROUP)
+
+
+def get_message_level(line) -> str:
+    return re.match(GENERAL_MESSAGE_PATTERN, line).group(MESSAGE_LEVEL_GROUP)
+
+
 class LogsReader:
     """
     TODO add parsing of XMLs and JSONs and validate it by user defined schemas
-    TODO add user-defined regexp patterns
-
     """
-    common_pattern = re.compile(
-        "^\[(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}.\d{3}): \[((?:trivia|verbose|info|warning|error))\] "
-        "'(.*)' (.*.cs) (\d{1,4}) .* \(Process id=(\d{1,9})\) \(Thread id=(\d{1,3})\)\]: (.*)")
-
-    commence_pattern = re.compile(
-        "^\[(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}.\d{3}): \[((?:trivia|verbose|info|warning|error))\] '(.*)' (.*.cs) "
-        "(\d{1,4}) .* \(Process id=(\d{1,9})\) \(Thread id=(\d{1,3})\)\]: Commence: (.*)")
-
-    complete_pattern = re.compile(
-        "^\[(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}.\d{3}): \[((?:trivia|verbose|info|warning|error))\] '(.*)' (.*.cs) "
-        "(\d{1,4}) .* \(Process id=(\d{1,9})\) \(Thread id=(\d{1,3})\)\]: Complete: (.*)"
-    )
 
     def __init__(self, content: str):
         self.content = content
@@ -83,38 +92,31 @@ class LogsReader:
 
     def analyse(self):
         for line_no, line in enumerate(self.content.split('\n')):
-            if not self._is_valid_message(line):
+            if not is_valid_message(line):
+                if 'Commence' in line:
+                    raise RuntimeError()
+                if 'Complete' in line:
+                    raise RuntimeError()
+                if 'getVolumePairs' in line:
+                    raise RuntimeError()
+                if 'ValidateTargetGroups' in line:
+                    raise RuntimeError()
                 continue
 
-            if self._is_commence_message(line):
-                self.call_graph.commence(self._get_scope_name(line))
+            if is_commence_message(line):
+                self.call_graph.commence(get_scope_name(line))
                 self.call_graph.add_info(error=False)
                 self.call_graph.add_info(warning=False)
-            elif self._is_complete_message(line):
+            elif is_complete_message(line):
                 self.call_graph.complete()
 
-            msg_lvl = self._get_message_level(line)
+            msg_lvl = get_message_level(line)
 
             if msg_lvl == 'error':
                 self.call_graph.add_info(error=True)
 
             if msg_lvl == 'warning':
                 self.call_graph.add_info(warning=True)
-
-    def _is_valid_message(self, line: str):
-        return bool(re.match(self.common_pattern, line))
-
-    def _is_commence_message(self, line):
-        return bool(re.match(self.commence_pattern, line))
-
-    def _is_complete_message(self, line):
-        return bool(re.match(self.complete_pattern, line))
-
-    def _get_scope_name(self, line):
-        return re.match(self.common_pattern, line).group(4)
-
-    def _get_message_level(self, line):
-        return re.match(self.common_pattern, line).group(3)
 
 
 def main(logfile: str, export_file_name: str):
